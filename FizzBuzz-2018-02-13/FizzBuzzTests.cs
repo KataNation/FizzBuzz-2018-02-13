f39ae9cf8146950046245f763ae9340020b0f246
Jim Speaker <jim@IONTECH.org>
2018-02-14 20:33:40 -0800

    Push to Github

---
+++ FizzBuzz-2018-02-13/FizzBuzzTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FizzBuzz_2018_02_13
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test, Category("Unit")]
+        public void GivenFour_WhenAskingToString_ThenItShouldReturnFour()
+        {
+            // arrange // act // assert
+            $"{new Integer(4)}".Should().Be("4");
+        }
+
+        [Test, Category("Unit")]
+        public void GivenNine_WhenAskingToString_ThenItShouldReturnFizz()
+        {
+            // arrange // act // assert
+            $"{new Integer(9)}".Should().Be("fizz");
+        }
+
+        [Test, Category("Unit")]
+        public void GivenTen_WhenAskingToString_ThenItShouldReturnBuzz()
+        {
+            // arrange // act // assert
+            $"{new Integer(10)}".Should().Be("buzz");
+        }
+
+        [Test, Category("Unit")]
+        public void GivenThirty_WhenAskingToString_ThenItShouldReturnFizzBuzz()
+        {
+            // arrange // act // assert
+            $"{new Integer(30)}".Should().Be("fizzbuzz");
+        }
+    }
+
+    public class Integer
+    {
+        private readonly int _value;
+
+        public Integer(int value)
+        {
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            const string fizz = "fizz";
+            const string buzz = "buzz";
+
+            if (_value.IsDivisibleBy(15)) return $"{fizz}{buzz}";
+            if (_value.IsDivisibleBy(5)) return buzz;
+            if (_value.IsDivisibleBy(3)) return fizz;
+
+            return _value.ToString();
+        }
+    }
+
+    public static class IntExtensions
+    {
+        public static bool IsDivisibleBy(this int dividend, int divisor)
+        {
+            return dividend % divisor == 0;
+        }
+    }
+}
---
+++ FizzBuzz-2018-02-13/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz_2018_02_13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBuzz_2018_02_13")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("185c0740-0548-4a65-a90c-564a6c271221")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
